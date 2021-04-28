1d999e0afe628954b98d9a2f72aaf4aa3f4478db
myshyak <myshyak@gmail.com>
2021-04-28 13:39:29 +0300

    [ADD] app login_AzureAd

---
+++ login_AzureAd/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Microsoft Azure Oauth",
+    "version": "1.0",
+    "license": "AGPL-3",
+    "author": "Tuc Ltd",
+    "website": "",
+    'price': 0.00,
+    'currency': 'USD',
+    "summary": "Allow users to login using Azure AD.",
+    "depends": ["auth_oauth"],
+    'data': [
+        'data/auth_oauth_data.xml',
+    ],
+'images': ['static/description/icon.png'],
+
+
+}
